38fb587737e0d626af7a6174620dda9b0a5f344a
Conor Murnane <conormurnane11@gmail.com>
2020-10-30 17:18:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -13,9 +13,10 @@ def hopper
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      }
-    }
-
+      },
+      programmer_hash{:grace_hopper}
+ 		}
+    
 end
 
 def alan_kay_is_known_for
